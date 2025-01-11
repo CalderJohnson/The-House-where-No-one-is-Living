992e8996474b59c3c54c79bd28ef391862d14bb0
johnso1x@cs.uwindsor.ca <johnso1x@uwindsor.ca>
2025-01-11 14:13:41 -0500

    fix: Enemies don't travel upwards

--- Assets/CODE/knife.cs
+++ Assets/CODE/knife.cs
@@ -10,7 +10,7 @@ public class knife : MonoBehaviour
     public float attackRange = 1.5f;    // Range of the knife attack
     public float attackCooldown;   // Cooldown between attacks
 
-    //private float nextAttackTime = 0f;
+    private float nextAttackTime = 0f;
     float lastAttack;
 
     // Start is called before the first frame update
@@ -19,22 +19,23 @@ public class knife : MonoBehaviour
         lastAttack = Time.time - attackCooldown;
     }
 
-    // Update is called once per frame
-    // void Update()
-    // {
-    //     if (Input.GetKeyDown(KeyCode.Mouse0) && Time.time >= nextAttackTime)
-    //     {
-    //         PerformKnifeAttack();
-    //         nextAttackTime = Time.time + attackCooldown; // Set next attack time
-    //     }
-        
-    // }
+    //Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0) && Time.time >= nextAttackTime)
+        {
+            PerformKnifeAttack();
+            nextAttackTime = Time.time + attackCooldown; // Set next attack time
+        }
+
+    }
 
     public void PerformKnifeAttack()
     {
-        
-        if(Time.time-lastAttack>attackCooldown){ //cooldown system
-            
+
+        if (Time.time - lastAttack > attackCooldown)
+        { //cooldown system
+
             lastAttack = Time.time;
 
             Collider[] hitEnemies = Physics.OverlapSphere(attackPoint.position, attackRange);
@@ -51,36 +52,36 @@ public class knife : MonoBehaviour
             AttackEffect();
             return;
         }
-        else{
+        else
+        {
             //Debug.Log("dont attack");
             return;
         }
-        
-        
-
-
-
-        // Short range damage
-        // Collider[] hitEnemies = Physics.OverlapSphere(attackPoint.position, attackRange);
-        // foreach (Collider enemy in hitEnemies)
-        // {
-        //     if (enemy.CompareTag("Enemy"))
-        //     {
-        //         Debug.Log("Hit enemy: " + enemy.name);
-        //         enemy.GetComponent<EnemyController>().TakeDamage(20);
-        //     }
-        // }
-
-        // Visual effect
-        //GameObject knife = Instantiate(knifePrefab, attackPoint.position, Quaternion.identity);
-        //Destroy(knife, 1f); // Destroy the visual effect after 1 seconds
-        
     }
+
+
+
+
+
+    //    Short range damage
+    //    Collider[] hitEnemies = Physics.OverlapSphere(attackPoint.position, attackRange);
+    //    foreach (Collider enemy in hitEnemies)
+    //    {
+    //        if (enemy.CompareTag("Enemy"))
+    //        {
+    //            Debug.Log("Hit enemy: " + enemy.name);
+    //            enemy.GetComponent<EnemyController>().TakeDamage(20);
+    //        }
+    //    }
+
+    //    Visual effect
+    //    GameObject knife = Instantiate(knifePrefab, attackPoint.position, Quaternion.identity);
+    //    Destroy(knife, 1f); // Destroy the visual effect after 1 seconds
+
+    //}
 
     public void AttackEffect(){ //particle effects
         GameObject effect = Instantiate(knifePrefab,attackPoint.position,Quaternion.identity);
         Destroy(effect,1f);
     }
-
-
 }
