29af68e1e1df7a86aa65bc1d556bf66c482947ad
SuperCrafter888 <37378903+SuperCrafter888@users.noreply.github.com>
2025-02-05 16:29:01 -0500

    Door Changes to Michaels Test Room

--- Assets/CODE/DoorBehavior.cs
+++ Assets/CODE/DoorBehavior.cs
@@ -7,6 +7,9 @@ public class DoorScript : MonoBehaviour
     public Animator doorAnimator; // Assign the Animator for this door
     public DoorScript linkedDoor; // Reference to the other door's DoorScript
     private bool isDoorActive = true; // To prevent re-triggering while the door is in use
+    public AudioSource doorAudioSource;
+    public AudioClip openSound;   // Door sounds
+    public AudioClip closeSound;
     public float x = 0f;
     public float y = 0f;
     public float z = 0f;
@@ -26,7 +29,9 @@ public class DoorScript : MonoBehaviour
         // Open this door
         if (doorAnimator != null)
         {
-            doorAnimator.Play("DoorOpen");
+            doorAnimator.Play("DoorOpen");         
+            doorAudioSource.clip = openSound;       // Play open sound
+            doorAudioSource.Play();
         }
 
         // Open the linked door
@@ -34,14 +39,19 @@ public class DoorScript : MonoBehaviour
         {
             linkedDoor.doorAnimator.Play("DoorOpen");
         }
-        yield return new WaitForSeconds(0.3f); // Wait for the linked door to open
+        yield return new WaitForSeconds(0.1f); // Wait for the linked door to open
 
-        
+        doorAudioSource.clip = closeSound;         // Play close sound
+        doorAudioSource.Play();
+
+        yield return new WaitForSeconds(0.2f);
+
         GameObject player = GameObject.FindWithTag("Player");
         player.SetActive(false);
         player.transform.position = new Vector3(x, y, z);
         player.SetActive(true);
 
+        yield return new WaitForSeconds(0.1f); 
 
         // Close both doors
         if (doorAnimator != null)
@@ -50,7 +60,9 @@ public class DoorScript : MonoBehaviour
         }
         if (linkedDoor != null && linkedDoor.doorAnimator != null)
         {
+            
             linkedDoor.doorAnimator.Play("DoorClose");
+            
         }
         yield return new WaitForSeconds(1f); // Wait for the doors to close
 
