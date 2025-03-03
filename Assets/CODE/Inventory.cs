89f5d0fb13dbbd0ccd8339c63f8c7e03b1d5ee23
SuperCrafter888 <37378903+SuperCrafter888@users.noreply.github.com>
2025-03-03 16:31:18 -0500

    Added the start of a save system in my test room

--- Assets/CODE/Inventory.cs
+++ Assets/CODE/Inventory.cs
@@ -1,10 +1,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Inventory : MonoBehaviour
+public class Inventory : MonoBehaviour, IDataPersistence
 {
     public static Inventory Instance;
-    private List<string> items = new List<string>();
+    private List<string> items = new List<string>(); // Stores inventory items
+    private HashSet<string> collectedItems = new HashSet<string>(); // Tracks collected items
 
     private void Awake()
     {
@@ -14,16 +15,42 @@ public class Inventory : MonoBehaviour
             Destroy(gameObject);
     }
 
+    public void LoadData(GameData data)
+    {
+        items = new List<string>(data.inventoryItems); // Load inventory
+        collectedItems = new HashSet<string>(data.collectedItems); // Load collected items
+        DisplayInventory();
+    }
+
+    public void SaveData(ref GameData data)
+    {
+        data.inventoryItems = new List<string>(items); // Save inventory
+        data.collectedItems = new HashSet<string>(collectedItems); // Save collected items correctly
+    }
+
     public void AddItem(string itemName)
     {
-        items.Add(itemName);
-        Debug.Log("Added to inventory: " + itemName);
-        DisplayInventory();
+        if (!collectedItems.Contains(itemName)) // Prevent duplicate collection
+        {
+            items.Add(itemName);
+            collectedItems.Add(itemName); // Mark as collected
+            Debug.Log("Added to inventory: " + itemName);
+            DisplayInventory();
+        }
+        else
+        {
+            Debug.Log("Item already collected: " + itemName);
+        }
     }
 
     public bool HasItem(string itemName)
     {
         return items.Contains(itemName);
+    }
+
+    public bool HasCollectedItem(string itemName)
+    {
+        return collectedItems.Contains(itemName);
     }
 
     public void DisplayInventory()
