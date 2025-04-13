0aca27719c5172f453fa65f5ae29ae7872fc3f13
AmitozBrar <97923457+AmitozBrar@users.noreply.github.com>
2025-04-13 14:32:42 -0400

    Rowan now has sword and sorcery

--- Assets/SwordCollision.cs
+++ Assets/SwordCollision.cs
@@ -5,10 +5,18 @@ using UnityEngine;
 public class SwordCollision : MonoBehaviour
 {
     public int damage = 10; // Damage dealt by the projectile
+    public bool attackActive = false;
+
+    public void attackStart(){
+        attackActive = true;
+    }
+    public void attackEnd(){
+        attackActive = false;
+    }
 
     void OnTriggerEnter(Collider collision)
     {
-        if (collision.gameObject.CompareTag("Enemy"))
+        if (collision.gameObject.CompareTag("Enemy") && attackActive ==true)
         {
             collision.gameObject.GetComponent<Healthbar>().TakeDamage(damage);
             // Debug.Log("Ranged hit enemy");
